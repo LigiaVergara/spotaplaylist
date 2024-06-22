8b17ee4535fa202f05f68011f7efae2c7f08869f
LigiaVergara <147092166+LigiaVergara@users.noreply.github.com>
2024-06-22 17:01:18 +0200

    try to fix deployment

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,5 @@
 import NextAuth from "next-auth";
-import SpotifyProvider from "next-auth/providers/spotify";
-
-const scope =
-  "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-read-recently-played user-read-playback-state user-top-read user-modify-playback-state user-read-currently-playing user-follow-read playlist-read-private user-read-email user-read-private user-library-read playlist-read-collaborative";
-
-export const authOptions = {
-  providers: [
-    SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID!,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
-      authorization: {
-        params: { scope },
-      },
-      ...{ protection: "pkce" },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }: { token: any; account: any }) {
-      if (account) {
-        token.id = account.id;
-        token.expires_at = account.expires_at;
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: any; token: any }) {
-      session.user = token;
-      return session;
-    },
-  },
-};
+import authOptions from '../../../lib/configs/auth/authOptions'
 
 const handler = NextAuth(authOptions);
 
