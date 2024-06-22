bb50c5ca28c05e23e3494afeebe16452d1374f6a
LigiaVergara <147092166+LigiaVergara@users.noreply.github.com>
2024-06-22 15:04:29 +0200

    changing tracks

--- app/tracks.ts
+++ app/tracks.ts
@@ -1,32 +1,35 @@
 import { getSession } from "next-auth/react";
 
-export async function getTracks(user: any, artistID: string): Promise<any> {
-  try {
-    // Retrieve the session information using Next.js' getSession
-    const session = await getSession(user);
+export async function getTracks(
+  user: any,
+  artistID: string,
+  topN: number = 1
+): Promise<any> {
+    try {
+        // Retrieve the session information using Next.js' getSession
+        const session = await getSession(user);
 
-    // Construct the URL for fetching top tracks of the artist
-    const url = `https://api.spotify.com/v1/artists/${artistID}/top-tracks?market=DE&limit=3`;
+        // Construct the URL for fetching top tracks of the artist
+        const url =`https://api.spotify.com/v1/artists/${artistID}/top-tracks?market=DE&limit=3`⁠;
 
-    // Fetch the top tracks from Spotify API
-    const response = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${(session?.user as any).accessToken}`,
-      },
-    });
+        // Fetch the top tracks from Spotify API
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${(session?.user as any).accessToken}`,
+              },
+            }).then((res) => {
+        if (!res.ok) {
+            throw new Error(`⁠Error fetching top tracks: ${res.statusText}`⁠);
+        }
+        return res.json();
+        });
 
-    // Check if the fetch was successful
-    if (!response.ok) {
-      throw new Error(`Error fetching Top Tracks: ${response.statusText}`);
-    }
+        const tracks = response["tracks"].slice(0, topN);
 
-    // Parse the response body as JSON
-    const tracks = await response.json();
-
-    // Return the array of top tracks
-    return tracks.items;
-  } catch (error) {
-    console.error('Failed to get Top Tracks:', error);
-    throw error;
-  }
-}
+        return tracks.map((track: any) => {
+        return track["id"];
+        });
+    } catch (error) {
+        console.error("Failed to get Top Tracks:", error);
+        throw error;
+    }}
