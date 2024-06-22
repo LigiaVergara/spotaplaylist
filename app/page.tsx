780b07d35f2eb0290b13a66f5f48197d9c9850a0
souzadalmolinf <souzadalmolinf@gmail.com>
2024-06-22 12:27:52 +0200

    changed design, only show top artists

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Spot a Playlist
         </p>
       </div>
@@ -47,34 +47,33 @@ export default function Home() {
       <div>
         <h1>Festivals</h1>
 
-        {loading &&
-          <div>Loading...</div>
-        }
+        {loading && <div>Loading...</div>}
 
-        {error &&
-          <div>Error: {error}</div>
-        }
+        {error && <div>Error: {error}</div>}
 
         {!error && !loading &&
           <ul>
-            {/* {playlists.map((playlist: any) => (
-              <li key={playlist.id}>{playlist.name}</li>
-            ))} */}
-            {festivals.map((festival: any) => (
-              <li key={festival.name} className="mb-4">
-              <a href={festival.url} target="_blank" className="text-blue-500 underline">
-                {festival.name}
-              </a>
-              <div>
-              </div>
-              <h2 className="mt-2 text-lg font-semibold">Artists:</h2>
-              <ul className="list-disc list-inside ml-4">
-                {festival.artists.map((artist: string, index: number) => (
-                  <li key={index}>{artist}</li>
-                ))}
-              </ul>
-            </li>
-            ))}
+            {festivals.map((festival: any) => {
+              const topArtists = festival.artists.slice(0, 5);
+              const remainingArtistsCount = festival.artists.length - topArtists.length;
+
+              return (
+                <li key={festival.name} className="mb-6">
+                  <a href={festival.url} target="_blank" className="text-blue-500 underline">
+                    {festival.name}
+                  </a>
+                  <h2 className="mt-2 text-lg font-semibold">Top Artists:</h2>
+                  <ul className="list-disc list-inside ml-4">
+                    {topArtists.map((artist: string, index: number) => (
+                      <li key={index}>{artist}</li>
+                    ))}
+                    {remainingArtistsCount > 0 && (
+                      <li>+ {remainingArtistsCount} more</li>
+                    )}
+                  </ul>
+                </li>
+              );
+            })}
           </ul>
         }
       </div>
