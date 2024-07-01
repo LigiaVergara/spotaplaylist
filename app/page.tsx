ad97e8560ff81d5e9a4510600843b84b9cead629
LigiaVergara <147092166+LigiaVergara@users.noreply.github.com>
2024-07-01 16:28:29 +0200

    Adding better visualization

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   const [selectedCountry, setSelectedCountry] = useState<string>(""); // Initialize with an empty string
   const [uniqueCountries, setUniqueCountries] = useState<string[]>([]);
   const [topN, setTopN] = useState<number>(1);
+  const [SearchQuery, setSearchQuery] = useState<string>("");
   const [playlistName, setPlaylistName] = useState<string>("");
   const [selectedPlaylistIndex, setSelectedPlaylistIndex] = useState<number | null>(null);
   const [playlistCreatedMessage, setPlaylistCreatedMessage] = useState<string | null>(null);
@@ -33,6 +34,10 @@ export default function Home() {
     const value = parseInt(event.target.value, 10); // Parse input value as integer
     setTopN(value); // Update topN state
   };
+
+  const handleSearchQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  }
 
   const handlePlaylistSelection = (index: number) => {
     setSelectedPlaylistIndex(index === selectedPlaylistIndex ? null : index);
@@ -101,104 +106,69 @@ export default function Home() {
               Discover your next favorite playlist based on a festival around you!
             </p>
             {session ? (
-               <p className="text-green-600 font-semibold px-4 py-2 rounded-md shadow-md">
-               Connected to Spotify
-             </p>
-           ) : (
-            <button
-              onClick={() => signIn("spotify", { callbackUrl: "http://localhost:3000" })}
-              className="bg-white text-green-600 font-semibold px-4 py-2 rounded-md hover:bg-gray-100 shadow-md"
-            >
-              Connect Spotify
-            </button>
+              <p className="text-green-600 font-semibold px-4 py-2 rounded-md shadow-md">
+                Connected to Spotify
+              </p>
+            ) : (
+              <button
+                onClick={() => signIn("spotify", { callbackUrl: "http://localhost:3000" })}
+                className="bg-white text-green-600 font-semibold px-4 py-2 rounded-md hover:bg-gray-100 shadow-md"
+              >
+                Connect Spotify
+              </button>
             )}
           </div>
         </div>
       </div>
-  {session && (
-     <div className="z-10 w-full max-w-5xl flex flex-col items-center">
-        <div className="w-full flex justify-center mb-8">
-          <div className="flex items-center bg-gradient-to-r from-green-400 via-green-500 to-green-600 text-green-600 p-6 rounded-lg shadow-lg">
-            <div className="flex flex-col md:flex-row items-center w-full md:w-auto">
-              {/* Country selection dropdown */}
-              <div className="mb-4 md:mb-0 md:mr-4 flex-grow">
-                <label htmlFor="countrySelect" className="block text-lg font-semibold mb-2">
-                  Select Country:
-                </label>
-                <select
-                  id="countrySelect"
-                  value={selectedCountry} // Default to empty string if selectedCountry is undefined
-                  onChange={handleCountryChange}
-                  className="bg-white border border-gray-300 rounded-md px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent w-full"
-                >
-                  <option value="">All Countries</option>
-                  {uniqueCountries.map((country: string, index: number) => (
-                    <option key={index} value={country}>
-                      {country}
-                    </option>
-                  ))}
-                </select>
-              </div>
-
-              {/* Input for topN selection */}
-              <div className="flex flex-col w-full md:w-auto">
-                <label htmlFor="topNInput" className="block text-lg font-semibold mb-2">
-                  Number of Tracks:
-                </label>
-                <input
-                  type="number"
-                  id="topNInput"
-                  value={topN}
-                  onChange={handleTopNChange}
-                  className="bg-white border border-gray-300 rounded-md px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent w-full"
-                />
+      {session && (
+        <div className="z-10 w-full max-w-5xl flex flex-col items-center">
+          <div className="w-full flex justify-center mb-8">
+            <div className="flex items-center bg-gradient-to-r from-green-400 via-green-500 to-green-600 text-green-600 p-6 rounded-lg shadow-lg">
+              <div className="flex flex-col md:flex-row items-center w-full md:w-auto">
+                <div className="mb-4 md:mb-0 md:mr-4 flex-grow">
+                  <label htmlFor="countrySelect" className="block text-lg font-semibold mb-2">
+                    Select Country:
+                  </label>
+                  <select
+                    id="countrySelect"
+                    value={selectedCountry}
+                    onChange={handleCountryChange}
+                    className="bg-white border border-gray-300 rounded-md px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent w-full"
+                  >
+                    <option value="">All Countries</option>
+                    {uniqueCountries.map((country: string, index: number) => (
+                      <option key={index} value={country}>
+                        {country}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+  
+                <div className="flex flex-col w-full md:w-auto">
+                  <label htmlFor="topNInput" className="block text-lg font-semibold mb-2">
+                    Number of Tracks:
+                  </label>
+                  <input
+                    type="number"
+                    id="topNInput"
+                    value={topN}
+                    onChange={handleTopNChange}
+                    className="bg-white border border-gray-300 rounded-md px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent w-full"
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    )}
-    {session && (
-          <div>
-            <h1 className="text-3xl font-bold text-green-900 mb-6">Festivals</h1>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {jambase_festivals.map((festival: any, index: number) => {
-                if (selectedCountry && festival.country !== selectedCountry) {
-                  return null; // Skip rendering if country doesn't match
-                }
-
-                const topArtists = festival.artists.slice(0, 5);
-                const remainingArtistsCount = festival.artists.length - topArtists.length;
-
-                return (
-                  <div
-                    key={index}
-                    className={`block p-6 cursor-pointer max-w-sm bg-green-100 rounded-lg border border-green-300 shadow-md hover:bg-green-200 transition-colors ${
-                      selectedPlaylistIndex === index ? 'border-green-600' : ''
-                    }`}
-                    onClick={() => handlePlaylistSelection(index)}
-                  >
-                    <h2 className="text-2xl font-bold text-green-900 mb-2">{festival.name}</h2>
-                    <h3 className="text-lg font-semibold text-gray-800">Artists:</h3>
-                    <ul className="list-disc list-inside ml-4 text-gray-700">
-                      {topArtists.map((artist: string, artistIndex: number) => (
-                        <li key={artistIndex}>{artist}</li>
-                      ))}
-                      {remainingArtistsCount > 0 && (
-                        <li>+ {remainingArtistsCount} more</li>
-                      )}
-                    </ul>
-                  </div>
-                );
-              })}
-            </div>
-
-            {selectedPlaylistIndex !== null && (
-              <div className="flex items-center mt-4">
-                {/* Input for playlist name */}
-                <div className="flex flex-col w-full md:w-auto mr-4">
-                  <label htmlFor="playlistNameInput" className="block text-lg font-semibold mb-2">
+      )}
+      {session && (
+        <div className="z-10 w-full max-w-5xl flex flex-col items-center">
+          {selectedPlaylistIndex !== null && (
+            <div className="flex items-center mb-8 w-full">
+              {/* Input for playlist name and button */}
+              <div className="flex flex-col md:flex-row items-center w-full md:w-auto space-x-4">
+                <div className="flex flex-col md:flex-row items-center">
+                  <label htmlFor="playlistNameInput" className="block text-lg font-semibold mb-2 md:mb-0 md:mr-4">
                     Playlist Name:
                   </label>
                   <input
@@ -206,7 +176,7 @@ export default function Home() {
                     id="playlistNameInput"
                     value={playlistName}
                     onChange={handlePlaylistNameChange}
-                    className="bg-white border border-gray-300 rounded-md px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent w-full"
+                    className="bg-white border border-gray-300 rounded-md px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent w-full md:w-auto"
                   />
                 </div>
                 <button
@@ -216,20 +186,62 @@ export default function Home() {
                   Create Playlist
                 </button>
               </div>
-            )}
-            {isLoading && (
-              <div className="mt-4 p-4 bg-blue-200 text-blue-900 rounded-md shadow-md">
-                Creating playlist, please wait...
-              </div>
-            )}
-
-            {playlistCreatedMessage && (
-              <div className="mt-4 p-4 bg-green-200 text-green-900 rounded-md shadow-md">
-                {playlistCreatedMessage}
-              </div>
-            )}
+            </div>
+          )}
+          {isLoading && (
+            <div className="mt-4 p-4 bg-blue-200 text-blue-900 rounded-md shadow-md">
+              Creating playlist, please wait...
+            </div>
+          )}
+          {playlistCreatedMessage && (
+            <div className="mt-4 p-4 bg-green-200 text-green-900 rounded-md shadow-md">
+              {playlistCreatedMessage}
+            </div>
+          )}
+        </div>
+      )}
+      {session && (
+        <div>
+          <h1 className="text-3xl font-bold text-green-900 mb-6">Festivals</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {jambase_festivals.map((festival: any, index: number) => {
+              if (selectedCountry && festival.country !== selectedCountry) {
+                return null;
+              }
+  
+              const topArtists = festival.artists.slice(0, 5);
+              const remainingArtistsCount = festival.artists.length - topArtists.length;
+              const imageUrl = festival.image || '/Festivals.jpeg';
+              const formattedDate = `${festival.date.slice(0, 4)}-${festival.date.slice(4, 6)}-${festival.date.slice(6, 8)}`;
+  
+              return (
+                <div
+                  key={index}
+                  className={`flex p-6 cursor-pointer bg-green-100 rounded-lg border border-green-300 shadow-md hover:bg-green-200 transition-colors ${
+                    selectedPlaylistIndex === index ? 'border-green-600' : ''
+                  }`}
+                  style={{ height: '150px' }} // Adjust these values as needed
+                  onClick={() => handlePlaylistSelection(index)}
+                >
+                  <img src={imageUrl} alt={festival.name} className="w-1/4 h-auto rounded-lg mr-4" />
+                  <div className="flex flex-col justify-between w-3/4">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl font-bold text-green-900">{festival.name}</h2>
+                      <span className="text-gray-500">{formattedDate}</span>
+                    </div>
+                    <div className="text-sm text-gray-700 mt-2">
+                      <span className="font-semibold">Artists: </span>
+                      <span>
+                        {topArtists.join(' / ')}
+                        {remainingArtistsCount > 0 && ` / + ${remainingArtistsCount} more`}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
+        </div>
       )}
     </main>
-  );
-}
+  );}
